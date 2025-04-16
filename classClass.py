d4dbff8b2989ef5115934a2ffd5681a36ab6fec8
arshan <smallchef9@gmail.com>
2025-04-16 11:13:55 -0400

    class Class!

---
+++ classClass.py
@@ -0,0 +1,33 @@
+
+class Class:
+    def __init__(self, name, instructor, section, day, period, duration=1):
+        self.name = name
+        self.instructor = instructor
+        self.section = section
+        self.day = [int(d) for d in str(day)]
+        self.period = period
+        self.duration = duration
+    """Most parameters self explanatory, 
+    day parameter will be a string including ints 0-4 representing Monday-Friday,
+    periods will run from 0-7 inclusive,
+    duration defaults to 1, denotes how many periods a class runs,
+    Ex: Class1 = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day="024",period=5)
+    """
+    def __repr__(self):
+        return f"{self.name} (Sec {self.section}) with {self.instructor}"
+
+    def get_time_blocks(self):
+        """
+        Returns list of (day, period) tuples this class occupies.
+        """
+        blocks = []
+        for d in self.day:
+            for p in range(self.period, self.period + self.duration):
+                blocks.append((d, p))
+        return blocks
+
+    def to_display_string(self):
+        """
+        Display string for the schedule grid.
+        """
+        return f"{self.name}\n{self.instructor}\nSec {self.section}\nPeriod {self.period}"
---
+++ main.py
@@ -0,0 +1,3 @@
+from classClass import Class
+Class1 = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day= "024", period=5)
+print(str(Class1))
