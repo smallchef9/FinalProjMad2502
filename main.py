add892c6d4e6a9907b2c9b29ad9f26255e3e1e8c
elliana :) <elliana.kirby@ufl.edu>
2025-04-16 21:16:55 -0400

    Added some more classes :)

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from classClass import Class
-Class1 = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day= "024", period=5)
-print(str(Class1))
+
+comp_math = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day="024", period=5)
+prog_1 = Class(name="COP3502C", instructor="Ashish Aggarwal", section="10565", day="123", period=7, duration=2)
+prog_2 = Class(name="COP3503C", instructor="Fatemah Tavassoli", section="11444", day="024", period=2)
+prog_with_r = Class(name="STA3100", instructor="Thomas Ippolito", section="20154", day="024", period=7)
+
+print(str(comp_math))
