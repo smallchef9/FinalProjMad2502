302b62e5c3d25e2d4226d62a1c4ba9ba251cd6bc
elliana :) <elliana.kirby@ufl.edu>
2025-04-16 23:03:11 -0400

    Added even more classes, and separated them by subject

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from classClass import Class
+import numpy as np
 
+# programming classes
 comp_math = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day="024", period=5)
 prog_1 = Class(name="COP3502C", instructor="Ashish Aggarwal", section="10565", day="123", period=7, duration=2)
 prog_2 = Class(name="COP3503C", instructor="Fatemah Tavassoli", section="11444", day="024", period=2)
 prog_with_r = Class(name="STA3100", instructor="Thomas Ippolito", section="20154", day="024", period=7)
 
-print(str(comp_math))
+# math classes
+calc_1 = Class(name="MAC2311", instructor="STAFF", section="13464", day="024", period=4)
+calc_2 = Class(name="MAC2312", instructor="STAFF", section="13532", day="024", period=7)
+calc_3 = Class(name="MAC2313", instructor="STAFF", section="13613", day="024", period=8)
+lin_alg_1 = Class(name="MAS4105", instructor="STAFF", section="13330", day="0124", period=5)
+
+# science classes
+physics_1 = Class(name="PHY2053", instructor="STAFF", section="14666", day="13", period=5)
+physics_2 = Class(name="PHY2054", instructor="STAFF", section="14616", day="13", period=2)
+gen_chem_1 = Class(name="CHM2045", instructor="STAFF", section="10561", day="024", period=1)
+bio_1 = Class(name="BSC2010", instructor="David Oppenheimer", section="11154", day="024", period=2)
+
+schedule = np.empty((8, 5)) # making an empty numpy array with 8 rows (for the 8 periods) and 5 columns (for the days of the week)
