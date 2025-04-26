4357cb0bf0cd578f6f3ad1d4d093cb9435ab46aa
arshan <smallchef9@gmail.com>
2025-04-26 15:09:14 -0400

    class Class!

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from classClass import Class
 Class1 = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day= "024", period=5)
+print(Class1.to_display_string())
+print(Class1.get_time_blocks())
 print(str(Class1))
