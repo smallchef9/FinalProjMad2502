b4a26c452bb6efbd519e52c6a8945c807ab4090e
elliana :) <elliana.kirby@ufl.edu>
2025-04-28 14:56:39 -0400

    Added checks to see if a class was already in the schedule at that time and if the max number of credit hours has been reached.

--- main.py
+++ main.py
@@ -15,27 +15,27 @@ def find_instructor(ins_wanted, all_ins: list):
   return [each_ins for each_ins in all_ins if each_ins.get_instructor().upper() == ins_wanted.upper()]
 
 # programming classes
-comp_math = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day="024", period=5)
-prog_1 = Class(name="COP3502C", instructor="Ashish Aggarwal", section="10565", day="123", period=7, duration=2)
-prog_2 = Class(name="COP3503C", instructor="Fatemah Tavassoli", section="11444", day="024", period=2)
-prog_with_r = Class(name="STA3100", instructor="Thomas Ippolito", section="20154", day="024", period=7)
+comp_math = Class(name="MAD2502", instructor="Ross Ptacek", section="17336", day="024", period=5, credits = 3)
+prog_1 = Class(name="COP3502C", instructor="Ashish Aggarwal", section="10565", day="123", period=6, duration=2, credits = 4)
+prog_2 = Class(name="COP3503C", instructor="Fatemah Tavassoli", section="11444", day="024", period=2, credits = 4)
+prog_with_r = Class(name="STA3100", instructor="Thomas Ippolito", section="20154", day="024", period=7, credits = 3)
 
 # math classes
-calc_1 = Class(name="MAC2311", instructor="STAFF", section="13464", day="024", period=4)
-calc_2 = Class(name="MAC2312", instructor="STAFF", section="13532", day="024", period=6)
-calc_3 = Class(name="MAC2313", instructor="STAFF", section="13613", day="024", period=7)
-lin_alg_1 = Class(name="MAS4105", instructor="STAFF", section="13330", day="0124", period=5)
+calc_1 = Class(name="MAC2311", instructor="STAFF", section="13464", day="024", period=4, credits = 4)
+calc_2 = Class(name="MAC2312", instructor="STAFF", section="13532", day="024", period=6, credits = 4)
+calc_3 = Class(name="MAC2313", instructor="STAFF", section="13613", day="024", period=7, credits = 4)
+lin_alg_1 = Class(name="MAS4105", instructor="STAFF", section="13330", day="0124", period=5, credits = 3)
 
 # science classes
-physics_1 = Class(name="PHY2053", instructor="STAFF", section="14666", day="13", period=5)
-physics_2 = Class(name="PHY2054", instructor="STAFF", section="14616", day="13", period=2)
-gen_chem_1 = Class(name="CHM2045", instructor="STAFF", section="10561", day="024", period=1)
-bio_1 = Class(name="BSC2010", instructor="David Oppenheimer", section="11154", day="024", period=2)
+physics_1 = Class(name="PHY2053", instructor="STAFF", section="14666", day="13", period=5, credits = 3)
+physics_2 = Class(name="PHY2054", instructor="STAFF", section="14616", day="13", period=2, credits = 3)
+gen_chem_1 = Class(name="CHM2045", instructor="STAFF", section="10561", day="024", period=1, credits = 3)
+bio_1 = Class(name="BSC2010", instructor="David Oppenheimer", section="11154", day="024", period=2, credits = 3)
 
 # english classes
-english_1 = Class(name = "ENC1101", instructor = "Angela Brown", section = "27585", day = "13", period = 3)
-english_2 = Class(name = "ENC1102", instructor = "STAFF", section = "11916", day = "24",period = 4)
-english_3 = Class(name = "ENC1136", instructor = "STAFF", section = "16175", day = "024", period = 4)
+english_1 = Class(name = "ENC1101", instructor = "Angela Brown", section = "27585", day = "13", period = 3, credits = 3)
+english_2 = Class(name = "ENC1102", instructor = "STAFF", section = "11916", day = "24",period = 4, credits = 3)
+english_3 = Class(name = "ENC1136", instructor = "STAFF", section = "16175", day = "024", period = 4, credits = 3)
 
 
 classes_list = [comp_math, prog_1, prog_2, prog_with_r, calc_1, calc_2, calc_3, lin_alg_1, physics_1, physics_2, gen_chem_1, bio_1, english_1, english_2, english_3]
@@ -49,25 +49,35 @@ def schedule_maker(classes: list):
     Returns:
         schedule: a 8x5 list that represents the 5 days of the week and 8 class periods
     """
-    # schedule = np.zeros((8, 5)) # making an of zeros numpy array with 8 rows (for the 8 periods) and 5 columns (for the days of the week)
 
-    # I couldn't get the numpy array to work with the Class objects, so I made a list to add to instead (will try again later)
     schedule = [[None for col in range(5)] for row in range(8)] # making a list with 8 rows and 5 columns
+    total_credits = 0
 
     for clss in classes:
         if clss in classes_list:
             ind = classes_list.index(clss)
             time_blocks = classes_list[ind].get_time_blocks()
 
+            added_class = False
             for day, period in time_blocks:
-                schedule[int(period)][int(day)] = classes_list[ind].to_display_string()
+                if schedule[int(period)][int(day)] is None:
+                    schedule[int(period)][int(day)] = classes_list[ind].to_display_string()
+                    added_class = True
+                else:
+                    print(f"{clss} conflicts with another class, could not add to schedule.")
 
-    return schedule
+            if added_class:
+                total_credits += classes_list[ind].credits
+        if total_credits > 18:
+            print("Maximum credit hours reached.")
+
+    return schedule, total_credits
 
 # testing to see if the function works, should have period 5 of M,W,F with comp math and period 5 of T,TH with physics 1
-test_list = [comp_math, physics_1, english_1, calc_3] # the most desired class should be first, the least desired last
-test_schedule = schedule_maker(test_list)
+test_list = [comp_math, physics_1, english_1, calc_3, prog_1, prog_with_r] # the most desired class should be first, the least desired last
+test_schedule, creds = schedule_maker(test_list)
 
 headers = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
 print(tabulate(test_schedule, headers=headers, tablefmt = "fancy_grid")) # displays the schedule nicely
+print(f"You are signed up for {creds} credit hours.")
